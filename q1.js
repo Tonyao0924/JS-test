b6fc62252d2d04a2219b50d092736764cd4e4e23
Tonyao0924 <mingyaochen0924@gmail.com>
2023-05-15 21:48:51 +0800

    ans

--- q1.js
+++ q1.js
@@ -6,32 +6,37 @@ const factories = [
 ];
 
 //   1. Count Employees Number by Factory // => [ {name: 'BR1', count: 4}, ... ]
-const ans1 = factories.map(factory => ({
-    name: factory.name,
-    count: factory.employees.length
-}));
-console.log(ans1);
+function getEmployeesCountByFactory(factories) {
+    return factories.map(factory => {
+        return {
+            name: factory.name,
+            count: factory.employees.length,
+        }
+    })
+}
 
 //   2. Count Factories Number by Employee // => [ {employee: 'John', count: 2}, ... ]
-const factoriesCountByEmployee = {};
-factories.forEach(factory => {
-    factory.employees.forEach(employee => {
-        if (factoriesCountByEmployee[employee]) {
-            factoriesCountByEmployee[employee]++;
-        } else {
-            factoriesCountByEmployee[employee] = 1;
-        }
+function getFactoriesCountByEmployee(factories) {
+    const factoriesCountByEmployee = {};
+    factories.forEach(factory => {
+        factory.employees.forEach(employee => {
+            if (factoriesCountByEmployee[employee]) {
+                factoriesCountByEmployee[employee]++;
+            } else {
+                factoriesCountByEmployee[employee] = 1;
+            }
+        });
     });
-});
-const ans2 = Object.entries(factoriesCountByEmployee).map(([employee, count]) => ({
-    employee,
-    count
-}));
-console.log(ans2);
-
+    const ans2 = Object.entries(factoriesCountByEmployee).map(([employee, count]) => ({
+        employee,
+        count
+    }));
+    return ans2;
+}
 //   3. Order employees list by alphabetical order // =>   { name: "BR2", employees: ["Jessie", "John", "Karen"] }
-const ans3 = factories.map(factory => ({
-    name: factory.name,
-    employees: factory.employees.sort()
-})).sort((a, b) => a.name.localeCompare(b.name));
-console.log(ans3);
+function orderEmployeesByAlphabeticalOrder(factories) {
+    return factories.map(factory => ({
+        name: factory.name,
+        employees: factory.employees.sort()
+    })).sort((a, b) => a.name.localeCompare(b.name));
+}
